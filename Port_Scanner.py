4420784b4fd9aa016dd78bf3b24e1c1112fe4e67
Kyle Kim(@Zeromini) <zoomgo2@naver.com>
2018-12-03 22:52:31 +0900

    Update port Scanner Automation

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -1,9 +1,3 @@
-'''
-# Python Port Scanner Automation #
-# 개발일 / 버전 : `18.12.02 / V 1.0  #
-# 아이디어, 개발 : zeromini #
-'''
-
 import os
 import nmap
 import sys
@@ -19,7 +13,7 @@ def main():
 
 
 def file_read_scan():
-	#print('test')
+	print (' Read File and Scanning Ready....')
 	try:
 		r = open ('c:/iplist.txt', mode='rt')
 		f = open ('c:/result.csv','w')
@@ -27,6 +21,7 @@ def file_read_scan():
 		r.close()
 		f.close()
 		nm = nmap.PortScanner()
+		file_flag = 0 
 
 		for i in read_result:
 			print('Scanning Ip Address : '+i[0])
@@ -34,9 +29,26 @@ def file_read_scan():
 			result = nm.scan(hosts=i[0], ports='20-443', arguments='-sV')
 			with open('c:/result.csv','a') as f:		
 				f.writelines(nm.csv())
+					
+	
 
 	except:
 		pass
+def excel_change():
+	print( ' Changing Excel Result.... ')
+	read_excel = pd.read_csv('c:/result.csv')
+	#print(read_excel)
+	read_excel_replace = read_excel.replace('host;hostname;hostname_type;protocol;port;name;state;product;extrainfo;reason;version;conf;cpe',None)
+	read_excel_drop = read_excel_replace.dropna(axis=1)
+	#print(read_excel_drop)
+	read_excel_drop.to_excel('c:/final_result.xlsx')
+
+	#read_result = read_excel.drop('host;hostname;hostname_type;protocol;port;name;state;product;extrainfo;reason;version;conf;cpe',axis=1)
+	#print(read_result)
+	#read_excel_split = read_excel.split(';',expand=True)
+	#print(read_excel_split)
+
+
 
 
 '''
@@ -51,6 +63,6 @@ with open('result.csv','w') as f:
 
 if __name__ == '__main__':
     	file_read_scan()
-    	excel_result = pd.read_excel('c:/result.csv') 
-    	excel_split_result = excel_result.str.split(';', expand=True)
+    	excel_change()
+
     
